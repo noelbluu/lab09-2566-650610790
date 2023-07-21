05c793473dce5685f590cf3f0e63dc62a6fe5c4c
noelbluu <panida.stp@icloud.com>
2023-07-21 21:44:55 +0700

    update

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -23,7 +23,9 @@ export const Task = ({
       */}
       <span className={completed ? "text-decoration-line-through" : ""}>{title}</span>
       <button className="btn btn-success" onClick={doneBtnOnClick}>Done</button>
-      <button className="btn btn-danger" onClick={deleteBtnOnClick}>Delete</button>
+      <button className="btn btn-danger" onClick={deleteBtnOnClick}>
+        Delete
+      </button>
       
     </div>
   );
